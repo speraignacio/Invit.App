5ec2eb1d0d3eaf01d4e73a9eae34b5fbe8589821
speraignacio <speraignacio@gmail.com>
2023-04-10 15:22:47 +0200

    ignspera - agrego el check email al login, falta capturar excepciones

--- src/main/java/app/rest/invit/repositories/UserRepository.java
+++ src/main/java/app/rest/invit/repositories/UserRepository.java
@@ -1,11 +1,20 @@
 package app.rest.invit.repositories;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import app.rest.invit.entities.PostEntity;
 import app.rest.invit.entities.UserEntity;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
+   
+    @Query(value = "SELECT * FROM users p WHERE p.user_id = :userId", nativeQuery = true)
+    UserEntity findByUserID(@Param("userId") String userId);
 }
